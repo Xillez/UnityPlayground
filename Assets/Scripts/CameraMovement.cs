9057cee00bce569392e67c67d3eedebb898a202f
Xillez <kent.wincent46@gmail.com>
2018-10-16 23:46:31 +0200

    Added old files

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Transform target;
+    public Vector3 offset;
+    public float dampingMultiplier;
+    
+    void LateUpdate()
+    {
+        if (!target)
+            return;
+
+        transform.position = Vector3.Slerp(transform.position, target.position + offset, dampingMultiplier * Time.deltaTime);
+                        
+        transform.LookAt(target);
+    }
+}
