f96c37b2bf79cef2cc0f04212b1b9f3eea82f250
Xillez <kent.wincent46@gmail.com>
2018-10-17 22:58:45 +0200

    Made crude version of Editor Window UI layout

--- Assets/Scripts/CityGen/Editor/CityGenWindow.cs
+++ Assets/Scripts/CityGen/Editor/CityGenWindow.cs
@@ -1,39 +1,89 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
 public class CityGenWindow : EditorWindow
 {
+    public static readonly Color splitterColor = new Color(130.0f / 256.0f, 130.0f / 256.0f, 130.0f / 256.0f, 1.0f);
+    private float separation = 10.0f;
+
 	[MenuItem("Window/City Gen")]
     static void Init()
     {
         // Get existing open window or if none, make a new one:
-        EditorWindow.GetWindow<CityGenWindow>().Show();
+        CityGenWindow window = EditorWindow.GetWindow<CityGenWindow>();
+        window.titleContent.text = "City Gen";
+        window.Show();
     }
 
     private void OnGUI()
     {
-        //
+        EditorGUILayout.BeginHorizontal();
+        {
+            MakeCitySelectionMenu(new Rect(Vector2.zero.x, Vector2.zero.y, (this.position.width * 0.2f) - (separation / 2.0f), (this.position.height * 0.5f) - (separation / 2)));
+            MakeSplitter(new Rect((this.position.width * 0.2f) - (separation / 2.0f), Vector2.zero.y, separation, (this.position.height * 0.5f) - (separation / 2)));
+            MakeCityScenePreview(new Rect((this.position.width * 0.2f) + (separation / 2.0f), Vector2.zero.y, this.position.width * 0.40f - separation, this.position.height * 0.5f));
+            MakeSplitter(new Rect((this.position.width * 0.60f) - (separation / 2.0f), Vector2.zero.y, separation, (this.position.height * 0.5f) - (separation / 2)));
+            MakeCitySettings(new Rect((this.position.width * 0.60f) + (separation / 2.0f), Vector2.zero.y, this.position.width * 0.40f - (separation / 2.0f), this.position.height * 0.5f));
+        }
+        EditorGUILayout.EndHorizontal();
+        MakeSplitter(new Rect(Vector2.zero.x, (this.position.height * 0.5f) - (separation / 2.0f), this.position.width, separation));
+        MakeCityAssetsManager(new Rect(Vector2.zero.x, (this.position.height * 0.5f) + (separation / 2.0f), this.position.width, (this.position.height * 0.5f) - (separation / 2.0f)));
+        
     }
 
-    private void MakeCitySelectionMenu()
+    private void MakeCitySelectionMenu(Rect rect)
     {
-        //
+        MakeSection(rect, () =>
+        {
+            GUILayout.BeginScrollView(Vector2.zero);
+            {
+                GUILayout.TextField("Add buttons for loading and creating cities here!");
+            }
+            GUILayout.EndScrollView();
+        });
     }
 
-    private void MakeCityAssetsManager()
+    private void MakeCityScenePreview(Rect rect)
     {
-        //
+        MakeSection(rect, () =>
+        {
+            GUILayout.Button("TODO:");
+            GUILayout.TextField("Add scene preview previewing the city to generate");
+        });
     }
 
-    private void MakeCityScenePreview()
+    private void MakeCitySettings(Rect rect)
     {
-        //
+        MakeSection(rect, () =>
+        {
+            GUILayout.Button("TODO:");
+            GUILayout.TextField("Add inspector like city settings screen, where user can edit city properties");
+        });
     }
 
-    private void MakeCitySettings()
+    private void MakeCityAssetsManager(Rect rect)
     {
-        //
+        MakeSection(rect, () =>
+        {
+            GUILayout.Button("TODO:");
+            GUILayout.TextField("Add asset managment (like the tree addition on terrain tool)");
+        });
+    }
+
+    private void MakeSplitter(Rect rect)
+    {
+        GUI.Box(rect, ""); //, GUIStyle.none);
+    }
+
+    private void MakeSection(Rect rect, Action content)
+    {
+        GUILayout.BeginArea(rect);
+        {
+            content();
+        }
+        GUILayout.EndArea();
     }
 }
