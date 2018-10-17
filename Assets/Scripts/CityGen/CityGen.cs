573e724d21d41591e557f3064a13e3ceba21deb0
Xillez <kent.wincent46@gmail.com>
2018-10-17 22:59:24 +0200

    Minor changes to CityGen.cs

--- Assets/Scripts/CityGen/CityGen.cs
+++ Assets/Scripts/CityGen/CityGen.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CityGen : MonoBehaviour {
+public class CityGen : MonoBehaviour
+{
+    // Follow this: http://cybercritics-critic.blogspot.com/2015/08/procedural-city-generation-in-unity3d.html
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
