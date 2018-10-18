1a465f10244f81be30c6bd2b09ff99490b131741
Xillez <kent.wincent46@gmail.com>
2018-10-19 01:20:02 +0200

    Minor change in RoadNetwork.SubdivideRoads()

--- Assets/Scripts/CityGen/RoadNetwork.cs
+++ Assets/Scripts/CityGen/RoadNetwork.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class RoadNetwork
 {
-    float minBlockWidth = 0.01f;
+    //float minBlockWidth = 0.01f;
+    private int nrBranches;
     List<RoadSegment> segments = new List<RoadSegment>();
 
     public void AddRoadSegment(RoadSegment segment)
@@ -18,8 +19,13 @@ public class RoadNetwork
         for (int i = iterations; i >= 0; i--)
             for (int seg = 0; seg < this.segments.Count; seg++)
             {
-                Random.Range(0.0f, 1.0f);   // Random distance from generation point in percent. Used for subdivision.
-
+                float random = Random.Range(0.4f, 0.6f);   // Random distance from generation point in percent. Used for subdivision.
+                Vector3 oldSegmentDirection = this.segments[i].end - this.segments[i].start;
+                Mathf.Tan(((360.0f / nrBranches) / 2.0f) * Mathf.Deg2Rad) * Vector3.Magnitude(oldSegmentDirection * random);
+                //float randomLength = Random.Range();
+                Vector3 newSegmentDirection = Vector3.Cross(oldSegmentDirection, Vector3.up) * Random.Range()
+                Vector3 newSegmentStart = (oldSegmentDirection * random) + (newSegmentDirection * Random.Range(0.3f, 0.7f));
+                RoadSegment newSegment;
             }
     }
 }
