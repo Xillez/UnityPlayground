ab36a522638f5eba501fe831ae97e8cadb9b7e02
Xillez <kent.wincent46@gmail.com>
2018-11-25 22:45:26 +0100

    Renamed some stuff, plus maybe some minor changes

---
+++ Assets/Scripts/CityGen/RoadNetworkRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadNetworkRenderer : MonoBehaviour
+{
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+}
